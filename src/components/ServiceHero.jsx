81c3e8b7ddfc056a46a03b4eba35f627dc354006
mriitian <132086670+mriitian@users.noreply.github.com>
2024-06-16 23:07:23 +0530

    Delete src directory

--- src/components/ServiceHero.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ServiceHero() {
-  return (
-    <div className='about-hero'>
-        <h1 className='about-title'>Services</h1>
-    </div>
-  )
-}
