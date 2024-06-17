09df1291ee4d924f8849795f122a88b9b21fed80
mriitian <132086670+mriitian@users.noreply.github.com>
2024-06-18 00:41:25 +0530

    Delete client directory

--- client/src/components/AboutHero.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutHero() {
-  return (
-    <div className='about-hero'>
-        <h1 className='about-title'>About Us</h1>
-    </div>
-  )
-}
